ab4d23d8741d689a3eeeda8d8981e515c4be88a7
JonChan <759282337@ffcs>
2020-07-08 16:12:28 +0800

    zookeep 向zookeep注册服务

---
+++ cloud_pay_8004/src/main/java/com/example/cloud_pay_8004/service/PaymentService.java
@@ -0,0 +1,24 @@
+package com.example.cloud_pay_8004.service;
+
+import com.example.cloud_api_commons.entity.Payment;
+import com.example.cloud_pay_8004.dao.PaymentDao;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Jon Chan
+ * @date 2020/7/8 8:43
+ */
+@Service
+@AllArgsConstructor
+public class PaymentService {
+  private PaymentDao paymentDao;
+
+  public void create(Payment payment){
+    paymentDao.insert(payment);
+  }
+
+  public Payment getPaymentById(Long id){
+    return paymentDao.findById(id);
+  }
+}
