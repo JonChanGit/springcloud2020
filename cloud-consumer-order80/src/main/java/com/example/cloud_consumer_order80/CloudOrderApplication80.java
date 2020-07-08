bdd425f866ae1ba3ce517ad34a55385ef400624b
JonChan <759282337@ffcs>
2020-07-08 15:35:31 +0800

    eureka 显示IP (eureka end)

--- cloud-consumer-order80/src/main/java/com/example/cloud_consumer_order80/CloudOrderApplication80.java
+++ cloud-consumer-order80/src/main/java/com/example/cloud_consumer_order80/CloudOrderApplication80.java
@@ -2,10 +2,12 @@ package com.example.cloud_consumer_order80;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableDiscoveryClient
 public class CloudOrderApplication80 {
 
     public static void main(String[] args) {
--- cloud-consumer-order80/src/main/java/com/example/cloud_consumer_order80/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/example/cloud_consumer_order80/controller/OrderController.java
@@ -3,9 +3,13 @@ package com.example.cloud_consumer_order80.controller;
 import com.example.cloud_api_commons.entity.Payment;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 /**
  * @author Jon Chan
@@ -19,6 +23,9 @@ public class OrderController {
   public static final String PAYMENT_URL = "http://CLOUD-PAYMENT-SERVICE";
   private RestTemplate restTemplate;
 
+  private DiscoveryClient discoveryClient;
+
+
   @PostMapping(value = "/consumer/payment/create")
   public ResponseEntity<Void> create(@RequestBody Payment payment) {
     return restTemplate.postForEntity(PAYMENT_URL + "/payment/create", payment, Void.class);
@@ -27,5 +34,27 @@ public class OrderController {
   @GetMapping("/consumer/payment/get/{id}")
   public ResponseEntity<Payment> getPayment(@PathVariable("id") Long id) {
     return restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, Payment.class);
+  }
+
+  /**
+   * 使用代码
+   * @return
+   */
+  @GetMapping(value = "/payment/discovery")
+  public Object discovery() {
+    // 好像不加@EnableDiscoveryClient也可以
+   List<String> services = discoveryClient.getServices();
+
+    for (String serviceId : services) {
+      log.info("*****element:" + serviceId);
+      List<ServiceInstance> instances = discoveryClient.getInstances(serviceId);
+      for (ServiceInstance instance : instances) {
+        log.info(instance.getServiceId() + "\t" + instance.getHost() +
+                "\t" + instance.getPort() + "\t" + instance.getUri());
+        log.info("{}",instance.getMetadata());
+      }
+    }
+
+    return this.discoveryClient;
   }
 }
