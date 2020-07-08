b86fad75c44f41bdd5f282932db7864d26a62d6f
JonChan <759282337@ffcs>
2020-07-08 11:18:13 +0800

    eureka 集群配置与注册

---
+++ cloud_eureka_server_7001/src/main/java/com/example/cloud_eureka_server_7002/EurekaMain7001.java
@@ -0,0 +1,18 @@
+package com.example.cloud_eureka_server_7002;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author Jon Chan
+ * @date 2020/7/8 10:15
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7001 {
+
+  public static void main(String[] args) {
+    SpringApplication.run(EurekaMain7001.class,args);
+  }
+}
--- cloud_eureka_server_7002/src/main/java/com/example/cloud_eureka_server_7002/EurekaMain7002.java
+++ cloud_eureka_server_7002/src/main/java/com/example/cloud_eureka_server_7002/EurekaMain7002.java
@@ -1,4 +1,4 @@
-package com.example.cloud_eureka_server_7001;
+package com.example.cloud_eureka_server_7002;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,9 +10,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  */
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaMain7001 {
+public class EurekaMain7002 {
 
   public static void main(String[] args) {
-    SpringApplication.run(EurekaMain7001.class,args);
+    SpringApplication.run(EurekaMain7002.class,args);
   }
 }
