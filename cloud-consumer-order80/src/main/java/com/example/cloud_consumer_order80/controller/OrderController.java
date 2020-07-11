bd9bfafdc04ab7f328c82bc8c7fb093dc0990504
JonChan <jonchan.hv@outlook.com>
2020-07-11 16:33:59 +0800

    客服端测 超时访问或运行时异常，演示降级

--- cloud-consumer-order80/src/main/java/com/example/cloud_consumer_order80/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/example/cloud_consumer_order80/controller/OrderController.java
@@ -2,6 +2,8 @@ package com.example.cloud_consumer_order80.controller;
 
 import com.example.cloud_api_commons.entity.Payment;
 import com.example.cloud_consumer_order80.rpc.PaymentRpcService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.client.ServiceInstance;
@@ -24,13 +26,25 @@ public class OrderController {
   private PaymentRpcService rpcService;
 
   @GetMapping(value = "/ok/{id}")
-  ResponseEntity<String> ok(@PathVariable("id") Long id){
+  ResponseEntity<String> ok(@PathVariable("id") Long id) {
     return rpcService.ok(id);
   }
 
+  @HystrixCommand(fallbackMethod = "paymentInfo_TimeoutFallbackMethod", commandProperties = {
+    @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "2000")
+  })
   @GetMapping(value = "/timeout/{id}")
-  ResponseEntity<String> timeOut(@PathVariable("id") Long id){
+  ResponseEntity<String> timeOut(@PathVariable("id") Long id) {
     return rpcService.timeOut(id);
+  }
+
+  /**
+   * 签名（除方法名）要和切入方法完全一致
+   * @param id
+   * @return
+   */
+  public ResponseEntity<String> paymentInfo_TimeoutFallbackMethod(@PathVariable("id") Long id) {
+    return ResponseEntity.ok("/(ToT)/我是消费者80，调用8001支付系统繁忙，请10秒钟后重新尝试、\t");
   }
 
 }
