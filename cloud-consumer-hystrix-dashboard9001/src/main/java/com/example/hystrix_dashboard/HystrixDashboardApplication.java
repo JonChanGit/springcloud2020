fafb85b398f98a2bceb62cb3d670335949fefc12
JonChan <jonchan.hv@outlook.com>
2020-07-12 06:24:43 +0800

    hystrix dashboard 创建 与配置

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/example/hystrix_dashboard/HystrixDashboardApplication.java
@@ -0,0 +1,17 @@
+package com.example.hystrix_dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author Jon Chan
+ * @date 2020/7/12 06:07
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(HystrixDashboardApplication.class,args);
+  }
+}
