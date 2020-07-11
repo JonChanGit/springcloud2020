f1954583ab66e69fbdd6d3d1db1b3120aa4cbefe
JonChan <jonchan.hv@outlook.com>
2020-07-11 15:49:21 +0800

    超时访问或运行时异常，演示降级

--- cloud_pay_8001/src/main/java/com/example/cloud_pay_8001/CloudPayApplication8001.java
+++ cloud_pay_8001/src/main/java/com/example/cloud_pay_8001/CloudPayApplication8001.java
@@ -2,10 +2,13 @@ package com.example.cloud_pay_8001;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+// 启用断路器
+@EnableCircuitBreaker
 public class CloudPayApplication8001 {
 
     public static void main(String[] args) {
--- cloud_pay_8001/src/main/java/com/example/cloud_pay_8001/service/PaymentService.java
+++ cloud_pay_8001/src/main/java/com/example/cloud_pay_8001/service/PaymentService.java
@@ -1,5 +1,7 @@
 package com.example.cloud_pay_8001.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,12 +17,26 @@ public class PaymentService {
   public String paymentInfo(Long id){
     return "Thread Name : " + Thread.currentThread().getName() + " \t id: "+ id;
   }
+
+  /**
+   * 超时访问或运行时异常，演示降级
+   * @param id
+   * @return
+   */
+  @HystrixCommand(fallbackMethod = "paymentInfo_TimeoutHandler",commandProperties = {
+    @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "3000")
+  })
   public String paymentInfo_Timeout(Long id){
+    System.out.printf(1/0 + "");
     try {
-      TimeUnit.SECONDS.sleep(3);
-    } catch (InterruptedException e) {
+      TimeUnit.SECONDS.sleep(7);
+    } catch (Exception e) {
       e.printStackTrace();
     }
-    return "Thread Name : " + Thread.currentThread().getName() + " \t id: "+ id;
+    return "Thread Name : " + Thread.currentThread().getName() + " \t id: "+ id + "\t paymentInfo_Timeout";
+  }
+
+  public String paymentInfo_TimeoutHandler(Long id) {
+    return "/(ToT)/调用接口超时或异常、\t" + "\t当前线程池名字" + Thread.currentThread().getName();
   }
 }
