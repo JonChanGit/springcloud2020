34e67b0a1662c18241590608d9c43c4f3adf39f9
JonChan <jonchan.hv@outlook.com>
2020-07-21 15:59:20 +0800

    Nacos-Discovery服务通过者注册

---
+++ cloud_pay_8001/readme.md
@@ -0,0 +1,11 @@
+# 服务通过者
+
+## 使用服务集群
+
+
+```shell script
+
+ java -Dserver.port=8802 -jar com.example.cloud_pay_8001.CloudPayApplication8001
+ java -Dserver.port=8803 -jar com.example.cloud_pay_8001.CloudPayApplication8001 
+
+```
--- cloud_pay_8001/src/main/java/com/example/cloud_pay_8001/controller/PaymentController.java
+++ cloud_pay_8001/src/main/java/com/example/cloud_pay_8001/controller/PaymentController.java
@@ -3,7 +3,9 @@ package com.example.cloud_pay_8001.controller;
 import com.example.cloud_api_commons.entity.Payment;
 import com.example.cloud_pay_8001.service.PaymentService;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,23 +13,26 @@ import org.springframework.web.bind.annotation.*;
  * @author Jon Chan
  * @date 2020/7/8 8:44
  */
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RestController
 @Slf4j
 public class PaymentController {
-  private PaymentService paymentService;
+  private final PaymentService paymentService;
+
+  @Value("${server.port}")
+  private String serverPort;
 
   @PostMapping(value = "/payment/create")
   public ResponseEntity<Void> create(@RequestBody Payment payment) {
     paymentService.create(payment);
-    log.info("****插入结果: SUCCESS");
+    log.info("****node {}  插入结果: SUCCESS", serverPort);
     return ResponseEntity.ok().build();
   }
 
   @GetMapping(value = "/payment/get/{id}")
   public ResponseEntity<Payment> getPaymentById(@PathVariable("id") Long id) {
     Payment payment = paymentService.getPaymentById(id);
-    log.info("****查询结果:" + payment);
+    log.info("****node {}  查询结果: {} ", serverPort, payment);
 
     return ResponseEntity.ok(payment);
   }
