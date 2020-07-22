d660b36a91e3034a28cd009e3d7908502a162c36
JonChan <jonchan.hv@outlook.com>
2020-07-22 09:03:26 +0800

    Nacos-sentinel 接入 sentinel

---
+++ cloud_pay_8001/src/main/java/com/example/cloud_pay_8001/controller/FlowLimitController.java
@@ -0,0 +1,29 @@
+package com.example.cloud_pay_8001.controller;
+
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author Jon Chan
+ * @date 2020/7/22 08:54
+ */
+@RestController
+@Slf4j
+public class FlowLimitController {
+
+  @GetMapping("/testA")
+  public String testA() {
+    return "----testA";
+  }
+
+  @GetMapping("/testB")
+  public String testB() {
+    return "----testB";
+  }
+}
